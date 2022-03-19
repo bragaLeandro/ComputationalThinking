bd0718600aa6420f26aecf22f8a016159d8f6715
bragaLeandro <101952699+bragaLeandro@users.noreply.github.com>
2022-03-19 19:30:54 -0300

    Add files via upload

---
+++ ExercisesList1/Ex2.py
@@ -0,0 +1,7 @@
+nome = input("Digite o seu nome: ")
+anoNascimento = input("Informe o ano de nascimento: ")
+anoNascimento = int(anoNascimento)
+idade = (2022 - anoNascimento)
+
+print(nome , "tem (ou terá)" , idade , "anos")
+
---
+++ ExercisesList1/Ex7.py
@@ -0,0 +1,5 @@
+a = int(input("Informe A: "))
+b = int(input("Informe B: "))
+c = int(input("Informe C: "))
+
+delta = (b**2 - 4*a*c)
