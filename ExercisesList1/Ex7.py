9f9c5467936a10849a3fc1456b169ac01b327cf8
Leandro Braga <rm94513@fiap.com.br>
2022-03-19 19:53:31 -0300

    Exercise 7 realized

--- ExercisesList1/Ex7.py
+++ ExercisesList1/Ex7.py
@@ -1,5 +1,9 @@
-a = int(input("Informe A: "))
-b = int(input("Informe B: "))
-c = int(input("Informe C: "))
+#Uma pessoa tem em seu guarda roupa x camisas, y calças e z pares de sapato. Escreva um algoritmo que calcula de quantas maneiras diferentes ele pode se vestir. Seu algoritmo deverá ler o número de camisas, o número de calças e o número de pares de sapato.
 
-delta = (b**2 - 4*a*c)
+quant_camisas = int(input("Quantas camisas você possui? "))
+quant_calcas = int(input("Quantas calças você possui? "))
+quant_sapatos = int(input("Quantos pares de sapato você possui? "))
+
+combinacao = (quant_camisas * quant_calcas * quant_sapatos)
+
+print("Baseado em suas roupas, você pode se vestir de " + str(combinacao) + " maneiras diferentes")
