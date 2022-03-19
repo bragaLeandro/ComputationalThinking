0b909b0c3cf811ac9e97cacf9b6fb0176dcaf433
bragaLeandro <101952699+bragaLeandro@users.noreply.github.com>
2022-03-19 19:30:37 -0300

    Create Ex1.py

---
+++ ExercisesList1/Ex1.py
@@ -0,0 +1,6 @@
+#Escreva um algoritmo em Python que recebe seu nome e sobrenome, depois de ler as informações mostra na tela o sobrenome e depois o nome
+
+nome = input("Digite o seu nome: ")
+sobrenome = input("Digite o seu sobrenome: ")
+
+print("O seu sobrenome é " + sobrenome + "\nO seu nome é " + nome)
