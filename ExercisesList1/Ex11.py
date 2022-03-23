cd4fa858ee69891da05e3392a352230c4e561193
bragaLeandro <rm94513@fiap.com.br>
2022-03-22 21:16:32 -0300

    Ex11 changes

--- ExercisesList1/Ex11.py
+++ ExercisesList1/Ex11.py
@@ -1,19 +1,27 @@
 # O RM de um aluno da FIAP é composto por 5 dígitos. Sua tarefa é escrever um algoritmo que recebe um RM e retorna a somatória de todos os dígitos do RM. Por exemplo, suponha que o aluno tenha o RM 56395, seu algoritmo deverá imprimir como saída 28 = 5 + 6 + 3 + 9 + 5. Dica: realize várias divisões e restos de divisões por 10.
 
-from zmq import ContextTerminated
-
-
 rm = int(input("Digite o seu RM: "))
+soma = 0
 
-unidade = rm%10
-conta_dezena = rm//10
-dezena = conta_dezena%10
-conta_centena = conta_dezena//10
-centena = conta_centena%10
-conta_milhar = conta_centena//10
-milhar = conta_milhar%10
-conta_dezena_milhar = conta_milhar//10
-dezena_milhar = conta_dezena_milhar%10
+resto = rm%10
+rm = rm//10
+soma = soma + resto
 
-soma_RM = (dezena_milhar + milhar + centena + dezena + unidade)
-print("A soma dos 5 números do seu RM dá:", soma_RM)
+resto = rm%10
+rm = rm//10
+soma = soma + resto
+
+resto = rm%10
+rm = rm//10
+soma = soma + resto
+
+resto = rm%10
+rm = rm//10
+soma = soma + resto
+
+resto = rm%10
+rm = rm//10
+soma = soma + resto
+
+
+print("A soma dos 5 números do seu RM é", soma)
