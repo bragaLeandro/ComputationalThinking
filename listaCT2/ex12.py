c0e115406170a0db2190e30958d4d4e33ed1734f
bragaLeandro <rm94513@fiap.com.br>
2022-03-29 22:58:33 -0300

    Exercicios realizados

--- listaCT2/ex12.py
+++ listaCT2/ex12.py
@@ -1,10 +1,16 @@
 #Desenvolva um algoritmo que informe se uma data é válida ou não. O algoritmo deverá ler 2 números inteiros, que representem o dia e o mês e informar se é um dia do mês válido. Desconsidere os casos de ano bissexto, ou seja, fevereiro têm 28 dias.
 
-meses_do_ano = [1 ,2 ,3 ,4 ,5, 6, 7, 8, 9, 10, 11, 12]
-mes_ate_31 = [1, 3, 5, 6, 8, 10, 12]
-mes_ate_30 = [4, 5, 9, 11]
-mes_ate_28 = 2
-
 dia  = int(input("Informe um dia do mês: "))
 mes = int(input("Informe um mês (1 a 12): "))
 
+if mes == 2 and dia <=28 and dia >=1:
+    print("Data válida")
+
+elif mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12 and dia <=31 and dia >=1:
+    print("Data válida")
+
+elif mes == 4 or mes == 6 or mes == 9 or mes == 1 and dia <=30 and dia >=1:
+    print("Data válida")
+
+else:
+    print("Data inválida")
---
+++ listaCT2/ex13.py
@@ -0,0 +1,25 @@
+#Agora, vamos acrescentar uma verificação de data os casos de ano bissexto, ou seja, o ano que fevereiro tem 29 dias. Um ano bissexto se:
+#a) o ano for divisível por 4
+#b) anos múltiplos de 100, não são bissextos
+#c) quando o ano for divisível por 400 ele é bissexto
+#d) as últimas regras prevalecem sobre as primeiras
+
+dia  = int(input("Informe um dia do mês: "))
+mes = int(input("Informe um mês (1 a 12): "))
+ano = int(input("Informe um ano (ex: 2002): "))
+
+if mes == 2 and dia <=28 and dia >=1:
+    print("Data válida")
+    
+elif ano%4 == 0 or ano%400 == 0 and mes == 2 and dia <=29 and dia >=1:
+    print("Data válida")
+
+elif mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12 and dia <=31 and dia >=1:
+    print("Data válida")
+
+elif mes == 4 or mes == 6 or mes == 9 or mes == 1 and dia <=30 and dia >=1:
+    print("Data válida")
+
+
+else:
+    print("Data inválida")
