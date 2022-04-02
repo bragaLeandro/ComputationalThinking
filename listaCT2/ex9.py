40420dd73f47e05dafcbfbd80250ff02381b6dbe
bragaLeandro <rm94513@fiap.com.br>
2022-04-01 22:22:19 -0300

    Correcao exerc bhaskara

--- listaCT2/ex9.py
+++ listaCT2/ex9.py
@@ -4,9 +4,10 @@ import math
 a = float(input("Informe o coefiente A: "))
 b = float(input("Informe o coefiente B: "))
 c = float(input("Informe o coefiente C: "))
-
-calculo_delta = b**2 - 4*a*c
-calculo_x1 = (-b + math.sqrt(calculo_delta))/2*a
-calculo_x2 = (-b - math.sqrt(calculo_delta))/2*a
+if a > 0:
+    calculo_delta = b**2 - 4*a*c
+if calculo_delta >= 0:
+    calculo_x1 = (-b + math.sqrt(calculo_delta))/2*a
+    calculo_x2 = (-b - math.sqrt(calculo_delta))/2*a
 
 print("O valor de x1 é:", calculo_x1, "e o valor de x2 é:", calculo_x2)
